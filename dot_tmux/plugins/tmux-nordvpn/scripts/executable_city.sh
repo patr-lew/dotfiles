428d05ea4c230891d81e9d803c71a426e749b5f8
Patryk Lewandowski <patryk.a.lewandowski@gmail.com>
2025-06-22 08:56:42 +0200

    empty chezmoi-driven repo

--- dot_tmux/plugins/tmux-nordvpn/scripts/executable_city.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-print_nordvpn_city() {
-    nordvpn status | sed -n -e 's/^City: \(.*\)/\1/p'
-}
-
-print_nordvpn_city
--- dot_tmux/plugins/tmux-nordvpn/scripts/executable_country.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-print_nordvpn_country() {
-    nordvpn status | sed -n -e 's/^Country: \(.*\)/\1/p'
-}
-
-print_nordvpn_country
